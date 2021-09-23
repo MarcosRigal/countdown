0369f865083200d62aa6063558cf9486bed4b79b
MarcosRigal <riveragavilanmarcos@gmail.com>
2021-09-23 10:31:43 +0200

    DONE

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import Countdown from 'react-countdown';
+
+const Finished = () => (
+  <span>IT'S THE DAY :P</span>
+);
+
+const renderer = ({ total, days, hours, minutes, seconds }) => {
+  if (total) {
+    // Render a countdown
+    return (
+      <span>
+      {days} DAYS
+      <br></br>
+      <br></br>
+      {hours} HRS
+      <br></br>
+      <br></br>
+      {minutes} MIN
+      <br></br>
+      <br></br>
+      {seconds} SEC
+      </span>
+    );
+  } else {
+    // Render a finished state
+    return <Finished />;
+  }
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>TIME UNTIL ELIO'S COMEBACK:</h1>
+      <Countdown date={new Date(2021,10,31)} renderer={renderer}/>
       </header>
     </div>
   );
